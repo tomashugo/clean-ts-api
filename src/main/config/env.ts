54da386a3916df4d3fd8f9edbc4eeb72e2743e37
Tomás Hugo <tomas.hugo@gmail.com>
2023-08-23 17:48:06 -0300

    feat: add SignUpFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://0.0.0.0:27017/clean-node-api',
   port: process.env.PORT ?? 5050
 }
