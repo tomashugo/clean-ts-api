abbfdf64ae51682f0631b93c565462cb85bbfe55
Tomás Hugo <tomas.hugo@gmail.com>
2023-08-25 17:56:56 -0300

    feat: add LogMongoRepository to SignupFactory

--- src/infra/db/mongodb/log-repository/log.spec.ts
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -26,6 +26,6 @@ describe('Log Mongo Repository', () => {
     const sut = makeSut()
     await sut.logError('any_error')
     const count = await errorCollection.countDocuments()
-    expect(count).toBe(0)
+    expect(count).toBe(1)
   })
 })
