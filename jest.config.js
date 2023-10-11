d308e483fc76567444ea3480137a0915d951559f
Tomás Hugo Almeida <tomas.hugo@gmail.com>
2023-10-10 23:04:28 -0300

    refactor: rename signup routes with login routes

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ const config = {
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
-  testEnvironment: 'node',
+  //testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
