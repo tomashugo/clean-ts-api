a9f32750f6284f6d34de7553d95d633fe484c7af
Tomás Hugo Almeida <tomas.hugo@gmail.com>
2023-10-12 22:14:09 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -15,9 +15,19 @@ describe('Jwt Adapter', () => {
         expect(signSpy).toHaveBeenCalledWith({ id: 'any_id'}, 'secret')
     })
 
+    
     test('Should return a token on sign success', async () => {
         const sut = new JwtAdapter('secret')
         const accessToken = await sut.encrypt('any_id')
         expect(accessToken).toBe('any_token')
+    })
+
+    test('Should throw if sign throws', async () => {
+        const sut = new JwtAdapter('secret')
+        jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+            throw new Error()
+        })
+        const promise = sut.encrypt('any_id')
+        await expect(promise).rejects.toThrow()
     })
 })
