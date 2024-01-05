3f4ab15b2cc2aaefae2452a4e294fa6f715d413b
Tomás Hugo <tomas.hugo@gmail.com>
2024-01-05 16:15:29 -0300

    feat: ensure DbAddAccount calls LoadAccountByEmailRepository with correct email

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,19 +1,23 @@
-import { type AccountModel, type AddAccount, type AddAccountModel, type AddAccountRepository, type Hasher } from './db-add-account-protocols'
+import { 
+  type LoadAccountByEmailRepository,
+  type AccountModel, 
+  type AddAccount, 
+  type AddAccountModel, 
+  type AddAccountRepository, 
+  type Hasher 
+} from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
   constructor (
     private readonly hasher: Hasher,
-    private readonly addAccountRepository: AddAccountRepository
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   ) { }
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
+    await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
     const hashedPassword = await this.hasher.hash(accountData.password)
     const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
     return account
-    // return await new Promise(resolve => {
-    //   resolve(
-    //     account
-    //   )
-    // })
   }
 }
--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -1,7 +1,5 @@
-import { LogMongoRepository } from '../../../../infra/db/mongodb/log/log-mongo-repository'
 import { SignUpController } from '../../../../presentation/controllers/signup/signup-controller'
 import { type Controller } from '../../../../presentation/protocols'
-import { LogControllerDecorator } from '../../../decorators/log-controller-decorator'
 import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
 import { makeDbAddAccount } from '../../use-cases/add-acount/db-add-account-factory'
 import { makeDbAuthentication } from '../../use-cases/authentication/db-authentication-factory'
@@ -11,6 +9,4 @@ export const makeSignUpController = (): Controller => {
   const dbAddAccount = makeDbAddAccount()
   const signupController = new SignUpController(dbAddAccount, makeSignUpValidation(), makeDbAuthentication())
   return makeLogControllerDecorator(signupController)
-  // const logMongoRepository = new LogMongoRepository()
-  // return new LogControllerDecorator(signUpController, logMongoRepository)
 }
