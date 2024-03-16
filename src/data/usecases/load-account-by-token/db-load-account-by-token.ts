1b4da5869a97c625c3d7392a5f0771c4c0d86b2e
Tomás Hugo <tomas.hugo@gmail.com>
2024-03-16 20:47:59 -0300

    test: ensure DbLoadAccountByToken returns null if Decrypter returns null

--- src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -5,7 +5,7 @@ import { AccountModel } from '../add-account/db-add-account-protocols'
 export class DbLoadAccountByToken implements LoadAccountByToken {
   constructor (private readonly decrypter: Decrypter) {}
 
-  async load(accessToken: string, role?: string): Promise<AccountModel> {
+  async load(accessToken: string, role?: string): Promise<AccountModel|null> {
     await this.decrypter.decrypt(accessToken)
     return new Promise(resolve => resolve(null))
   }
